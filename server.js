658fcb562ccf3979a8f9c34b3b15cf5bc240ec39
Vladislav Lobanov <vladislav.lobanov@outlook.com>
2021-07-23 01:18:14 +0200

    version for pres onFriday

--- server.js
+++ server.js
@@ -37,6 +37,15 @@ app.use(function (req, res, next) {
     next();
 });
 
+if (process.env.NODE_ENV == "production") {
+    app.use((req, res, next) => {
+        if (req.headers["x-forwarded-proto"].startsWith("https")) {
+            return next();
+        }
+        res.redirect(`https://${req.hostname}${req.url}`);
+    });
+}
+
 app.use(requireLoggedInUser);
 
 app.use(petition);
